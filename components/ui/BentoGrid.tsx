3dd724a698365873a4c35baca67a03eb47133958
Mnet007 <67646792+Mnet007@users.noreply.github.com>
2024-08-08 17:19:54 +0100

    lil change

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -109,7 +109,7 @@ export const BentoGridItem = ({
 
                 {id == 3 && (
                     <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-                        <div className="flex flex-col gap-3 lg:gap-8">
+                        <div className="flex flex-col gap-1 lg:gap-8">
                             {['React.js', 'Next.js', 'Typescript','Three.js'].map((item) =>(
                                 <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base 
                                 opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
@@ -118,7 +118,7 @@ export const BentoGridItem = ({
                             ))}
                             <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
                         </div>
-                        <div className="flex flex-col gap-3 lg:gap-8">
+                        <div className="flex flex-col gap-1 lg:gap-8">
                         <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
                             {['Express.js', 'Node.js', 'TailwindCSS','Web3.js'].map((item) =>(
                                 <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base 
